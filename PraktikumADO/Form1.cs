a07b3587826b6006b8ea2be092c502aaf45a5f5c
Bagaswidhi <Bagaswidhi10@gmail.com>
2026-03-13 15:00:55 +0700

    Menambahkan isi fungsi Insert Program studi

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -133,7 +133,20 @@ namespace PraktikumADO
 
         private void insrtPS_Click(object sender, EventArgs e)
         {
-
+            try
+            {
+                Koneksi();
+                conn.Open();
+                string query = "INSERT INTO ProgramStudi (KodeProdi, NamaProdi) VALUES ('MI01', 'Manajemen Informatika')";
+                cmd = new SqlCommand(query, conn);
+                int hasil = cmd.ExecuteNonQuery();
+                MessageBox.Show("Jumlah baris terpengaruh : " + hasil);
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
